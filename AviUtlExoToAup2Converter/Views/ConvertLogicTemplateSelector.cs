2ad8d41fa0c7725f402627e270502e1bf593163b
量産型量産機 <GurZun1019@gmail.com>
2025-09-09 22:18:03 +0900

    画面表示のたたき台まで

---
+++ AviUtlExoToAup2Converter/Views/ConvertLogicTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AviUtlExoToAup2Converter.Views
+{
+    public class ConvertLogicTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            try
+            {
+                return (DataTemplate)Application.Current.FindResource(item.GetType().Name);
+            }
+            catch(Exception e)
+            {
+                return base.SelectTemplate(item, container);
+            }
+        }
+    }
+}
